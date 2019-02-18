18b87fa9fdc53f8f802e8b575f390de7a405e023
rpeschke <peschke@hawaii.edu>
2019-02-18 11:17:38 -1000

    added support for copying csv files to build folder

--- vhdl_make_symulation.py
+++ vhdl_make_symulation.py
@@ -13,6 +13,9 @@ def vhdl_make_simulation_intern(entity,BuildFolder = "build/"):
     OutputBuild = OutputPath + "make.sh"
     outputExe =  entity + ".exe"
     inputPath =  entity+ ".prj"
+    
+    CSV_readFile=OutputPath+Entity+".csv" 
+    CSV_writeFile=OutputPath+Entity+"_out.csv" 
 
     OutputRun = OutputPath + "run.sh"
 
@@ -29,15 +32,23 @@ def vhdl_make_simulation_intern(entity,BuildFolder = "build/"):
         f.write("fuse -intstyle ise -incremental -lib secureip -o " + outputExe + " -prj " +  inputPath + "  work." + entity)
 
 
-    with open(OutputRun,'w') as f:
+    with open(OutputRun,'w',newline="") as f:
+        f.write('if [ "$1" != "" ]; then \n')
+        f.write("   cp -f $1 " +CSV_readFile+ "  \n")
+        f.write("fi \n")
         f.write("cd " +OutputPath+ "  \n")
         f.write("source ./make.sh \n")
         f.write("./"+ outputExe + " -intstyle ise -tclbatch isim.cmd  \n")
         f.write("cd -  \n")
+        f.write('if [ "$2" != "" ]; then \n')
+        f.write("   cp -f " +CSV_writeFile+ " $2  \n")
+        f.write("fi \n")
 
 
 
 def vhdl_make_simulation(Entity,BuildFolder = "build/"):
+
+    
     DataBaseFile=BuildFolder+"DependencyBD"
     vhdl_parse_folder(Folder= ".",DataBaseFile=DataBaseFile)
     vhdl_get_dependencies(Entity,DataBaseFile=DataBaseFile)
