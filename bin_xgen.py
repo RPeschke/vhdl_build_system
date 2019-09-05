efb336e7d6c22548cf84a9147f060cee33065039
rpeschke <peschke@hawaii.edu>
2019-09-05 13:21:00 -1000

    updated code gen module

--- bin_xgen.py
+++ bin_xgen.py
@@ -7,17 +7,16 @@ sys.path.insert(0,parentdir)
 
 from  vhdl_build_system.vhdl_make_test_bench   import *
 from  vhdl_build_system.vhdl_parser            import *
+from  vhdl_build_system.vhdl_xgen              import *
 
 def main():
     parser = argparse.ArgumentParser(description='Generate Packages')
     parser.add_argument('--path',        help='Path to where the build system is located',default="build/")
-    parser.add_argument('--packageName', help='',default="axiStream")
-    parser.add_argument('--arguments',   help='',default="")
+    parser.add_argument('--packageName', help='',default="xgen_axiStream_32")
+    parser.add_argument('--scriptName',   help='',default="axiStream")
     args = parser.parse_args()
-    flist = getListOfFiles(".", "*/" + args.packageName + ".py")
-    print(flist)
-    if len(flist) > 0:
-        print(flist[0])
+    make_xgen(args.scriptName,args.packageName,args.path)
+
 
 if __name__== "__main__":
     main()
--- vhdl_xgen.py
+++ vhdl_xgen.py
@@ -1,4 +1,24 @@
-def vhdl_xgen_make_package(packageName,OutputPath="build/xgen/"):
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from  vhdl_build_system.vhdl_make_test_bench   import *
+from  vhdl_build_system.vhdl_parser            import *
+
+from  vhdl_make_test_bench   import *
+from  vhdl_parser            import *
+
+def make_xgen(scriptName,PackageName,path="build/"):
+    flist = getListOfFiles(".", "*/" + scriptName + ".py")
+    print(flist)
+    if len(flist) > 0:
+        print(flist[0])
+        line = "python3  " + flist[0] + " --OutputPath " +path +"/xgen/" +  PackageName + ".vhd --PackageName " + PackageName
+        print(line)
+        os.system(line)
+
+def vhdl_xgen_make_package(packageName,OutputPath="build/"):
     fileName = OutputPath+packageName+".vhd"
 
 
