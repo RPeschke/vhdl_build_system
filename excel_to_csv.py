0bd35937fd2ab57ab93e21370cc5992371914006
rpeschke <rp40@hawaii.edu>
2021-10-29 11:16:50 -1000

    update

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -3,10 +3,12 @@ from shutil import copyfile
 import pandas as pd
 import argparse
 
-def Convert2CSV123(XlsFile,Sheet,OutputFile):
+def Convert2CSV123(XlsFile,Sheet,OutputFile,drop):
 
     data_xls = pd.read_excel(XlsFile, Sheet, index_col=None)
-
+    print(data_xls.columns)
+    #if drop:
+    #    data_xls = data_xls.drop(['globals_clk', 'globals_rst', 'globals_reg_address', 'globals_reg_value', 'globals_reg_new_value'],axis=1 )
     data_xls.to_csv(OutputFile, encoding='utf-8',index =False, sep=" ") 
 
 
@@ -16,13 +18,17 @@ def main():
     parser.add_argument('--OutputCSV',    help='Path to the output',default="test.csv")
     parser.add_argument('--InputXLS',   help='Path to the input file',default="TXWaveFormOutputCompact.xlsm")
     parser.add_argument('--SheetXLS',   help='Sheet inside the XLS file',default="Simulation_Input")
+    #parser.add_argument('--Drop',   help='drops columns from data frame',default='globals_clk,globals_rst,globals_reg_address,globals_reg_value,globals_reg_new_value')
+    parser.add_argument('--ExportFor',   help='simulation or hardware',default='simulation')
+
     args = parser.parse_args()
+    drop = args.ExportFor == "hardware"
     print("\nargs.InputXLS: ",args.InputXLS,"\nargs.SheetXLS",args.SheetXLS,"\nargs.OutputCSV",args.OutputCSV)
     if args.InputXLS.split(".")[-1].lower() == "csv":
         #skip converting just copying
         copyfile( args.InputXLS , args.OutputCSV)
     else:
-        Convert2CSV123(args.InputXLS,args.SheetXLS,args.OutputCSV)
+        Convert2CSV123(args.InputXLS,args.SheetXLS,args.OutputCSV,drop)
     print("done Converting")
 
 if __name__== "__main__":
